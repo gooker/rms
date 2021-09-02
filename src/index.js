bfdcad018b726abfd6d457fed74664b644f1d60d
caona <na.cao@mushiny.com>
2021-09-02 11:06:13 +0800

    style: header的内容 涉及的gloabl/user的model 以及组件

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import dva from '@/utils/dva';
 import App from '@/components/pages/Portal/App';
 import appModel from '@/models/app';
 import taskModel from '@/models/task';
+import userModel from '@/models/user';
+import globalModel from '@/models/global';
 import './global.less';
 
 // 1. 初始化Dva对象
@@ -15,6 +17,8 @@ window.g_app = app;
 // 3. 注册 Model
 app.model(appModel);
 app.model(taskModel);
+app.model(userModel);
+app.model(globalModel);
 
 // 4. 启动
 const DvaProvider = app.create();
