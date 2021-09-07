6de1e86a4fa7d60542951c81c3ab44c306310860
周成成 <chengcheng.zhou@mushiny.com>
2021-09-07 10:32:52 +0800

    类型：[新增] 描述：国际化数据完善

--- src/locales/en-US/common.js
+++ src/locales/en-US/common.js
@@ -1,10 +1,19 @@
 export default {
-  'app.common.RequestLoginTimeoutContent': '请重新登录，获取相关权限(3秒后自动退出)',
-  'app.common.networkError': '请求失败，网络连接可能出现异常',
-  'app.common.systemError': '操作失败，系统没有返回任何提示',
-  'app.common.systemHint': '系统提示',
-  'app.common.operationFinish': '操作成功',
-  'app.common.operationFailed': '操作失败',
+  // 通用表单必填提醒
+  'app.required.agvId': '请选择小车ID',
+
+  // 通用提醒内容
+  'app.tip.getMapFail': '获取地图数据失败',
+  'app.tip.getMapListFail': '获取地图列表失败',
+  'app.tip.getAgvListFail': '获取小车列表失败',
+  'app.tip.RequestLoginTimeoutContent': '请重新登录，获取相关权限(3秒后自动退出)',
+  'app.tip.networkError': '请求失败，网络连接可能出现异常',
+  'app.tip.systemError': '操作失败，系统没有返回任何提示',
+  'app.tip.systemHint': '系统提示',
+  'app.tip.operationFinish': '操作成功',
+  'app.tip.operationFailed': '操作失败',
+
+  // 通用页面标识内容
   'app.common.tableRecord': '共计 {count} 条记录',
   'app.common.type': '类型',
   'app.common.status': '状态',
@@ -118,16 +127,49 @@ export default {
   'app.taskStatus.Cancel': '已取消',
   'app.taskStatus.Error': '失败',
 
-  // Common Tip
-  'app.required.agvId': '请选择小车ID',
-  'app.tip.getListFail': '获取小车列表失败',
   'app.taskAction.cancel.confirm': '确定取消已选择的任务吗?',
   'app.taskAction.cancel.success': '取消任务成功',
   'app.taskAction.cancel.fail': '取消任务失败',
 
+  // 充电桩状态
+  'app.chargeManger.ERROR': '错误',
+  'app.chargeManger.OFFLINE': '离线',
+  'app.chargeManger.AVAILABLE': '可用',
+  'app.chargeManger.ASSIGNED': '已分配',
+  'app.chargeManger.CHARGING': '充电中',
+  'app.chargeManger.CONNECTING': '对接中',
+  'app.chargeManger.CONNECTED': '对接成功',
 
   // times
   'app.common.day': '天',
   'app.common.hours': '时',
   'app.common.minutes': '分',
+
+  // 上传
+  'app.common.upload.dragUploadedFileToThisArea': '将上传的文件单击或拖拽到此区域',
+
+  // 地图
+  'app.map.map': '地图',
+  'app.map.logicArea': '逻辑区',
+  'app.map.routeArea': '路线区',
+  'app.map.point': '点位',
+  'app.map.path': '路线',
+  'app.map.view': '显示',
+  'app.map.feature': '功能',
+  'app.map.notGoAway': '不可走点',
+  'app.map.storagePoint': '存储点',
+  'app.map.followingPoint': '跟车点',
+  'app.map.waitingPoint': '等待点',
+  'app.map.notStay': '不可逗留点',
+  'app.map.fetchTask': '接任务点',
+  'app.map.workstation': '工作站',
+  'app.map.charger': '充电桩',
+  'app.map.elevator': '电梯',
+  'app.map.channel': '通道',
+  'app.map.restArea': '休息区',
+  'app.map.obstacle': '障碍物',
+  'app.map.commonPoint': '通用功能点',
+  'app.map.dumpPoint': '抛物点',
+  'app.map.intersection': '交汇点',
+  'app.map.roller': '滚筒站点',
 };
