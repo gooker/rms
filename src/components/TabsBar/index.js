90dd691e07d9a0e05dc65766328955f95f8d2950
周成成 <chengcheng.zhou@mushiny.com>
2022-02-24 08:56:27 +0800

    # 优化Tab栏左右侧按钮交互体验

--- src/components/TabsBar/index.js
+++ src/components/TabsBar/index.js
@@ -1,16 +1,19 @@
 import React, { memo, useEffect, useRef, useState } from 'react';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import { throttle, findIndex } from 'lodash';
+import { throttle } from 'lodash';
 import { connect } from '@/utils/RmsDva';
 import TabButton from '@/components/TabsBar/TabButton';
 import styles from './tabsBar.module.less';
 
 const TabsBar = (props) => {
-  const { dispatch, tabInfo, tabs, activeTab, currentActiveTabIndex } = props;
+  const { dispatch, tabInfo, tabs, activeTab } = props;
   const [width, setWidth] = useState();
+  const [leftDisabled, setLeftDisabled] = useState(); // 左按钮禁用
+  const [rightDisabled, setRightDisabled] = useState(); // 右按钮禁用
   const domRef = useRef();
 
   useEffect(() => {
+    // 监听容器Resize事件
     const layoutContentDOM = document.getElementById('layoutContent');
     const resizeObserver = new ResizeObserver(
       throttle(() => {
@@ -21,36 +24,40 @@ const TabsBar = (props) => {
           _width = width - 200;
         }
         setWidth(_width);
+        onScroll();
       }, 500),
     );
     resizeObserver.observe(layoutContentDOM);
+
+    // 监听Scroll事件
+    domRef.current.onscroll = throttle(onScroll, 200);
 
     return () => {
       resizeObserver.disconnect();
     };
   }, []);
 
+  useEffect(() => {
+    onScroll();
+  }, [tabs]);
+
+  function onScroll() {
+    const scrollLeft = domRef.current.scrollLeft;
+    const clientWidth = domRef.current.clientWidth;
+    const scrollWidth = domRef.current.scrollWidth;
+
+    setLeftDisabled(scrollLeft === 0);
+    setRightDisabled(clientWidth === scrollWidth || scrollWidth - clientWidth === scrollLeft);
+  }
+
   function leftScroll() {
-    if (currentActiveTabIndex !== 0) {
-      const currentIndex = currentActiveTabIndex - 1;
-      dispatch({ type: 'menu/saveActiveTab', payload: tabs[currentIndex].path });
-      scrollToActiveTab(currentIndex);
-    }
+    domRef.current.scrollTo(0, 0);
   }
 
   function rightScroll() {
-    if (currentActiveTabIndex !== tabs.length - 1) {
-      const currentIndex = currentActiveTabIndex + 1;
-      dispatch({ type: 'menu/saveActiveTab', payload: tabs[currentIndex].path });
-      scrollToActiveTab(currentIndex);
-    }
-  }
-
-  function scrollToActiveTab(currentIndex) {
-    const currentActiveTabDOM = domRef.current.children[currentIndex];
-    currentActiveTabDOM.scrollIntoView({
-      behavior: 'smooth',
-    });
+    const clientWidth = domRef.current.clientWidth;
+    const scrollWidth = domRef.current.scrollWidth;
+    domRef.current.scrollTo(scrollWidth - clientWidth, 0);
   }
 
   return (
@@ -60,8 +67,8 @@ const TabsBar = (props) => {
         onClick={leftScroll}
         style={{
           marginRight: 3,
-          background: currentActiveTabIndex !== 0 ? '#fff' : '#9f9f9f',
-          cursor: currentActiveTabIndex !== 0 ? 'pointer' : 'not-allowed',
+          background: !leftDisabled ? '#fff' : '#9f9f9f',
+          cursor: !leftDisabled ? 'pointer' : 'not-allowed',
         }}
       >
         <LeftOutlined />
@@ -87,8 +94,8 @@ const TabsBar = (props) => {
         onClick={rightScroll}
         style={{
           marginLeft: 3,
-          background: currentActiveTabIndex !== tabs.length - 1 ? '#fff' : '#9f9f9f',
-          cursor: currentActiveTabIndex !== tabs.length - 1 ? 'pointer' : 'not-allowed',
+          background: !rightDisabled ? '#fff' : '#9f9f9f',
+          cursor: !rightDisabled ? 'pointer' : 'not-allowed',
         }}
       >
         <RightOutlined />
@@ -96,11 +103,7 @@ const TabsBar = (props) => {
     </div>
   );
 };
-export default connect(({ menu }) => {
-  const { tabs, activeTab } = menu;
-  return {
-    tabs,
-    activeTab,
-    currentActiveTabIndex: findIndex(tabs, { path: activeTab }),
-  };
-})(memo(TabsBar));
+export default connect(({ menu }) => ({
+  tabs: menu.tabs,
+  activeTab: menu.activeTab,
+}))(memo(TabsBar));
