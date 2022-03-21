47e09cf6cb62c6840f2eb6dce78cefcf7171b4c1
周成成 <chengcheng.zhou@mushiny.com>
2022-03-21 17:21:22 +0800

    * 版本: 3.1 * 任务：912 * 更新描述：菜单icon

--- src/packages/Portal/components/Portal/Portal.js
+++ src/packages/Portal/components/Portal/Portal.js
@@ -1,16 +1,12 @@
 import React, { memo } from 'react';
 import { connect } from '@/utils/RmsDva';
 import { AppCode } from '@/config/config';
-import PortalEntry from './PortalEntry';
+import CommonPortal from './PortalEntry';
 import styles from './Portal.module.less';
-
-const { I18N, SSO, XIHE, Mixrobot, LatentLifting, Tote, Sorter, ForkLifting } = PortalEntry;
-
-const PortalWidth = 25;
-const PortalHeight = 25;
+import { formatMessage } from '@/utils/util';
 
 const Portal = (props) => {
-  const { dispatch, isAdmin, grantedAPP, currentApp, customLogo } = props;
+  const { dispatch, isAdmin, grantedAPP, currentApp } = props;
 
   async function checkoutApp(appCode) {
     dispatch({ type: 'global/saveCurrentApp', payload: appCode });
@@ -36,117 +32,15 @@ const Portal = (props) => {
   function renderAppInfo() {
     return (
       <>
-        {getAppList().map((appCode) => {
-          if (appCode === AppCode.LatentLifting) {
-            return (
-              <LatentLifting
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-          if (appCode === AppCode.Tote) {
-            return (
-              <Tote
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-          if (appCode === AppCode.ForkLifting) {
-            return (
-              <ForkLifting
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-          if (appCode === AppCode.Sorter) {
-            return (
-              <Sorter
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-
-          if (appCode === AppCode.I18N) {
-            return (
-              <I18N
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-          if (appCode === AppCode.SSO) {
-            return (
-              <SSO
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-          if (appCode === AppCode.XIHE) {
-            // 如果是自定义的Logo，就显示M
-            if (customLogo) {
-              return (
-                <Mixrobot
-                  key={appCode}
-                  currentApp={currentApp}
-                  checkoutApp={checkoutApp}
-                  name={appCode}
-                  width={PortalWidth}
-                  height={PortalHeight}
-                />
-              );
-            }
-            return (
-              <XIHE
-                key={appCode}
-                currentApp={currentApp}
-                checkoutApp={checkoutApp}
-                name={appCode}
-                width={PortalWidth}
-                height={PortalHeight}
-              />
-            );
-          }
-
-          return (
-            <XIHE
-              key={appCode}
-              currentApp={currentApp}
-              checkoutApp={checkoutApp}
-              name={appCode}
-              width={PortalWidth}
-              height={PortalHeight}
-            />
-          );
-        })}
+        {getAppList().map((appCode) => (
+          <CommonPortal
+            key={appCode}
+            code={appCode}
+            name={formatMessage({ id: `app.module.${appCode}` })}
+            currentApp={currentApp}
+            checkoutApp={checkoutApp}
+          />
+        ))}
       </>
     );
   }
