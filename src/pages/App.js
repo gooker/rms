45fae45f770be7fd9f15eee85ccb4d48710f37e3
周成成 <chengcheng.zhou@mushiny.com>
2022-07-15 12:06:36 +0800

    * 版本:1.0.0 * 任务: * 更新描述:修复访问本地nginx环境，dev环境被取消情况下登录异常

--- src/pages/App.js
+++ src/pages/App.js
@@ -17,6 +17,7 @@ class App extends Component {
 
   async componentDidMount() {
     try {
+      // TODO: 开发环境不需要自定义环境功能
       window.dbContext = await openDB();
       const defaultAPI = requestAPI();
       const customEnvironments = await selectAllDB(window.dbContext);
